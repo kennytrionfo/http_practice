3cd44d079582a45a5d729e9a0e9793ec5400c58e
Kenny T <ktrionfo@integracore.com>
2016-01-27 16:28:59 +0000

    rearranged the controller structure to match the way James doe it

--- people/app.js
+++ people/app.js
@@ -2,16 +2,14 @@
 	var app = angular.module('httpApp', ['ngResource']);
 
 	/////////////////////////========CONTROLLER=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
-	app.controller('PeopleCtlr', function($scope, $resource){
-		$scope.peopleData = $resource(
-			'data.json' 
-		);
 
+	function PeopleCtrl($scope, $resource){
+		$scope.peopleData = $resource('data.json');
 	  $scope.peopleData.get().$promise.then(function(response) {
 	  	$scope.peopleInfo = response.records;
-	 	  console.log($scope.peopleInfo );
+	 	  // console.log($scope.peopleInfo );
 	  });
-
-	});
+	}
+	app.controller('PeopleCtrl', PeopleCtrl);
 
 })();
