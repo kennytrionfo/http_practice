637c0d957986d128dac6518fba320c6455ae9384
Kenny T <ktrionfo@integracore.com>
2016-01-27 16:00:51 +0000

    took out the array that's passed into the controller

--- people/app.js
+++ people/app.js
@@ -1,27 +1,17 @@
 (function(){
+	var app = angular.module('httpApp', ['ngResource']);
 
+	/////////////////////////========CONTROLLER=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	app.controller('people', function($scope, $resource){
+		$scope.peopleData = $resource(
+			'file:///C:/rails_projects/kennysandbox/angular_stuff/http_practice/people/data.json' 
+		);
 
-var app = angular.module('httpApp', ['ngResource']);
+	  $scope.peopleData.get().$promise.then(function(response) {
+	  	$scope.peopleInfo = response.records;
+	 	  console.log($scope.peopleInfo );
+	  });
 
-/////////////////////////========CONTROLLER=======\\\\\\\\\\\\\\\\\\\\\\\\\\\
-app.controller('people', ['$scope', '$resource', function($scope, $resource){
-	$scope.peopleData = $resource(
-		'file:///C:/rails_projects/kennysandbox/angular_stuff/http_practice/people/data.json' 
-		// {
-		// 	callback: "JSON_CALLBACK"
-		// }, 
-		// {
-		// 	get: 
-		// 	{
-		// 		method: "JSONP"
-		// 	}
-		// }
-	);
-	 $scope.peopleData.get().$promise.then(function(response) {
-	 	$scope.peopleInfo = response.records;
-	 	console.log($scope.peopleInfo );
-	 });
-
-}]);
+	});
 
 })();
